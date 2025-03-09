71a18d43ec583c6a03870c914deff227125c3525
Altanfiz <altanfizgenity@gmail.com>
2025-03-09 12:43:01 +0700

    style base simple quick add button

--- src/lib/QuickAddButton.svelte
+++ src/lib/QuickAddButton.svelte
@@ -7,4 +7,18 @@
   }
 </script>
 
-<button class="quick-add-button" onclick={handleClick}>Add task</button>
+<button
+  id="quick-add-button"
+  class="flex gap-2 items-center justify-center"
+  onclick={handleClick}
+>
+  <div class="icon w-5 h-5 text-primary">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      ><path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path></svg
+    >
+  </div>
+  <div class="text">Add Task</div>
+</button>
