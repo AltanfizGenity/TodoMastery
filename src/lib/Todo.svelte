e5bf4c62490fb6e09ca754a73a36131e63557ee7
Altanfiz <altanfizgenity@gmail.com>
2025-03-10 22:52:15 +0700

    add completed button on Todo

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -11,6 +11,8 @@
   function editTodo() {
     quickFormState.set({ editMode: true, isOpen: true, todoID: todo.id });
   }
+
+  function completeTodo() {}
 </script>
 
 {#if $quickFormState.isOpen && $quickFormState.todoID == todo.id}
@@ -19,6 +21,29 @@
   <div class="todo">
     <div class="title font-light">{todo.title}</div>
     <div class="actions">
+      <button
+        class="complete-todo-button"
+        onclick={completeTodo}
+        aria-label="complete button"
+      >
+        <svg
+          class="uncheck-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          ><path
+            d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z"
+          ></path></svg
+        ><svg
+          class="check-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          ><path
+            d="M4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12ZM12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM17.4571 9.45711L16.0429 8.04289L11 13.0858L8.20711 10.2929L6.79289 11.7071L11 15.9142L17.4571 9.45711Z"
+          ></path></svg
+        >
+      </button>
       <button
         class="delete-todo-button"
         onclick={removeTodo}
