f796020983d8756e5f1a7973a16f373273303706
Altanfiz <altanfizgenity@gmail.com>
2025-03-21 08:28:00 +0700

    Feature: Create and style date-action input

--- src/lib/components/forms/QuickForm.svelte
+++ src/lib/components/forms/QuickForm.svelte
@@ -56,12 +56,26 @@
 </script>
 
 <form onsubmit={handleSubmit} class="quick-add-form">
-	<input
-		type="text"
-		name="title"
-		placeholder="input task"
-		value={$quickFormState.editMode ? currentTodo.title : ''}
-	/>
+	<div class="input-group">
+		<input
+			type="text"
+			name="title"
+			placeholder="input task"
+			value={$quickFormState.editMode ? currentTodo.title : ''}
+		/>
+	</div>
+	<div class="action-input-group">
+		<button class="input-action timeline-input" type="button" title="Set a date">
+			<div class="icon">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+					><path
+						d="M9 1V3H15V1H17V3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H7V1H9ZM20 11H4V19H20V11ZM7 5H4V9H20V5H17V7H15V5H9V7H7V5Z"
+					></path></svg
+				>
+			</div>
+			<p class="text">Date</p>
+		</button>
+	</div>
 	<div class="form-controls">
 		<button type="button" onclick={closeForm} class="cancel-button">cancel</button>
 		<button type="submit" class="submit-button"
@@ -79,6 +93,32 @@
 		gap: var(--spacing-4);
 		border-radius: var(--radius-lg);
 		border: 0.0625rem solid var(--color-muted);
+	}
+
+	.action-input-group {
+		display: flex;
+		gap: var(--spacing-4);
+		align-items: center;
+	}
+
+	.input-action {
+		display: flex;
+		gap: var(--spacing-2);
+		align-items: center;
+		border: 1px solid var(--color-muted);
+		color: var(--color-text);
+		padding: var(--padding-1);
+		border-radius: var(--radius-lg);
+		font-size: var(--text-sm);
+
+		&:hover {
+			background-color: var(--color-background);
+		}
+
+		.icon {
+			width: var(--text-sm);
+			height: var(--text-sm);
+		}
 	}
 
 	.form-controls {
