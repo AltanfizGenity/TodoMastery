b62166b8cb726cf9dddf41a3930a7efce4a2037a
Altanfiz <altanfizgenity@gmail.com>
2025-05-01 19:56:35 +0700

    feature: add custom button super component

---
+++ src/lib/components/CustomButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	interface CustomButtonProps {
+		type: 'button' | 'submit' | 'reset' | null | undefined;
+		variant: 'primary' | 'secondary' | 'muted';
+		disabled: boolean;
+		class: string;
+		onclick?: (event: MouseEvent) => void;
+		[key: string]: any;
+	}
+
+	let { children, type, variant, disabled, class: extendClass }: CustomButtonProps = $props();
+</script>
+
+<button {type} class={`custom-button ${extendClass} ${variant}`} {disabled}>
+	{@render children()}
+</button>
