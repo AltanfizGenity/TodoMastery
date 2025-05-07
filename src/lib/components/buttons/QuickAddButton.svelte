7221e60e363e6d6a670e95d4598fab98b27102a0
Altanfiz <altanfizgenity@gmail.com>
2025-05-08 05:42:49 +0700

    refactor: make QuickAddButton reusable

--- src/lib/components/buttons/QuickAddButton.svelte
+++ src/lib/components/buttons/QuickAddButton.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-	import { quickFormState } from '$lib/stores/app.store';
-
-	function handleClick() {
-		quickFormState.set({ isOpen: true, editMode: false, todoID: '' });
-	}
+	let props: EventProps = $props();
 </script>
 
-<button class="quick-add-button icon-text-button" onclick={handleClick}>
+<button class="quick-add-button icon-text-button" {...props}>
 	<div class="icon">
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
 			><path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path></svg
