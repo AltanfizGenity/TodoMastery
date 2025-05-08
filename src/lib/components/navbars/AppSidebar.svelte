e8d446c027be3bbf3a72c523403692aa4548f3d9
Altanfiz <altanfizgenity@gmail.com>
2025-05-08 20:16:57 +0700

    feature: add functionality to some sidebar action

--- src/lib/components/navbars/AppSidebar.svelte
+++ src/lib/components/navbars/AppSidebar.svelte
@@ -1,6 +1,8 @@
 <script>
+	import { redirect } from '@sveltejs/kit';
 	import { AddCircleIcon, CompletedIcon, InboxIcon, SearchIcon, TodayIcon } from '../icons';
 	import Action from '../other/sidebar/Action.svelte';
+	import { goto } from '$app/navigation';
 
 	function openAddFormModal() {
 		console.log('hello');
@@ -11,9 +13,14 @@
 	<ul class="action-list">
 		<Action Icon={AddCircleIcon} text="Create task" isActive={false} />
 		<Action Icon={SearchIcon} text="Search" isActive={false} />
-		<Action Icon={InboxIcon} text="Inbox" isActive={true} />
-		<Action Icon={TodayIcon} text="Today" isActive={false} />
-		<Action Icon={CompletedIcon} text="Completed" isActive={false} />
+		<Action Icon={InboxIcon} text="Inbox" isActive={true} onclick={() => goto('/app/inbox')} />
+		<Action Icon={TodayIcon} text="Today" isActive={false} onclick={() => goto('/app/today')} />
+		<Action
+			Icon={CompletedIcon}
+			text="Completed"
+			isActive={false}
+			onclick={() => goto('/app/completed')}
+		/>
 	</ul>
 </aside>
 
---
+++ src/routes/app/today/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import TodoListView from '$lib/components/todo/TodoListView.svelte';
+	import QuickAddButton from '$lib/components/buttons/AddButton.svelte';
+	import QuickForm from '$lib/components/forms/QuickForm.svelte';
+	import CompletedTodoListView from '$lib/components/todo/CompletedTodoListView.svelte';
+	import { quickFormState } from '$lib/stores/app.store';
+	import { openDefaultQuickForm } from '$lib/utils/app.utils';
+</script>
+
+<div class="todo-mastery">
+	<main class="list-main-view">
+		<h1>Today</h1>
+		<TodoListView />
+		{#if $quickFormState.isOpen && !$quickFormState.editMode}
+			<QuickForm />
+		{:else}
+			<QuickAddButton onclick={openDefaultQuickForm} />
+		{/if}
+		<CompletedTodoListView />
+	</main>
+</div>
+
+<style>
+	.todo-mastery {
+		width: var(--full);
+		height: var(--full);
+		min-height: 100dvh;
+		padding: var(--padding-20);
+	}
+
+	.list-main-view {
+		width: 80%;
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+		gap: var(--spacing-4);
+	}
+</style>
