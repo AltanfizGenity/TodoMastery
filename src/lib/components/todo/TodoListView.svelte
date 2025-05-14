395b4b38e4c95e4945f1d421e23ff810f830da46
Altanfiz <altanfizgenity@gmail.com>
2025-05-15 06:56:17 +0700

    feature: make TodolistView dynamic

--- src/lib/components/todo/TodoListView.svelte
+++ src/lib/components/todo/TodoListView.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import { todoList } from '$lib/stores/app.store';
 	import TodoElement from './TodoElement.svelte';
+
+	interface TodolistViewProps extends ElementProps {
+		todos: Todo[];
+	}
+
+	const { todos }: TodolistViewProps = $props();
 </script>
 
 <div class="todo-list-view">
-	{#each $todoList.filter((todo) => !todo.isComplete) as todo}
+	{#each todos as todo}
 		<TodoElement {todo} />
 	{/each}
 </div>
