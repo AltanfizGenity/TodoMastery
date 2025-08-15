c7e7be58c4a20fa3b135d476c6b52db4f13df02e
Altanfiz <altanfizgenity@gmail.com>
2025-08-15 14:40:49 +0700

    refactor(todo): extract item element from page.svelte into component

---
+++ src/lib/components/TodoItem.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { todos } from '$lib/store/todo';
+
+	interface TodoItemProps {
+		todo: Todo;
+	}
+
+	let { todo }: TodoItemProps = $props();
+
+	function completeTodo(todoId: string) {
+		let updatedTodos = $todos.map((todo) => {
+			if (todo.id === todoId) {
+				return {
+					...todo,
+					completed: true
+				};
+			}
+
+			return todo;
+		});
+		todos.set(updatedTodos);
+	}
+</script>
+
+<div class="todo">
+	<h3 class={`${todo.completed ? 'line-through' : ''}`}>{todo.title}</h3>
+	<button class="cursor-pointer" onclick={() => completeTodo(todo.id)}>✅ Mark as complete</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import TodoItem from '$lib/components/TodoItem.svelte';
 	import { todos } from '$lib/store/todo';
 
 	let todoTitle = $state('');
@@ -12,32 +13,13 @@
 
 		todos.update((todos) => [...todos, newTodo]);
 	}
-
-	function makeTodoComplete(todoId: string) {
-		let updatedTodos = $todos.map((todo) => {
-			if (todo.id === todoId) {
-				return {
-					...todo,
-					completed: true
-				};
-			}
-
-			return todo;
-		});
-		todos.set(updatedTodos);
-	}
 </script>
 
 <div class="todo-container">
 	<h1>Todos</h1>
 	<div class="todo-list">
 		{#each $todos as todo}
-			<div class="todo">
-				<h3 class={`${todo.completed ? 'line-through' : ''}`}>{todo.title}</h3>
-				<button class="cursor-pointer" onclick={() => makeTodoComplete(todo.id)}
-					>✅ Mark as complete</button
-				>
-			</div>
+			<TodoItem {todo} />
 		{/each}
 	</div>
 	<form class="todo-form" onsubmit={handleSubmit}>
