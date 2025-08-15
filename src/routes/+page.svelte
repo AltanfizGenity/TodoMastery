be3c9b74dd3e7bc556db39f8253e94cb9b81b115
Altanfiz <altanfizgenity@gmail.com>
2025-08-15 13:57:37 +0700

    feature(MVP): todo stored on local store and displayed on view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
+	import { todos } from '$lib/store/todo';
+
 	let todoTitle = $state('');
 
 	function handleSubmit() {
-		todoTitle && alert('new todo: ' + todoTitle);
+		let newTodo: Todo = {
+			id: crypto.randomUUID(),
+			title: todoTitle,
+			completed: false
+		};
+
+		todos.update((todos) => [...todos, newTodo]);
 	}
 </script>
 
 <div class="todo-container">
 	<h1>Todos</h1>
-	<div class="todo-list"></div>
+	<div class="todo-list">
+		{#each $todos as todo}
+			<h3>{todo.title}</h3>
+		{/each}
+	</div>
 	<form class="todo-form" onsubmit={handleSubmit}>
 		<input type="text" placeholder="Add a todo" bind:value={todoTitle} />
 		<button type="submit">create new todo</button>
--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -1,11 +1,5 @@
 interface Todo {
 	id: string;
 	title: string;
-	isComplete: boolean;
-	date: LuxonTime | null;
-}
-
-interface QuickTodoEdit {
-	todoID: string;
-	isOpen: boolean;
+	completed: boolean;
 }
