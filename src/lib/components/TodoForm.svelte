ec86e8a019b96c9be9553a8dac682490dba0c3be
Altanfiz <altanfizgenity@gmail.com>
2025-08-15 15:35:43 +0700

    feat(todo): make title input required

--- src/lib/components/TodoForm.svelte
+++ src/lib/components/TodoForm.svelte
@@ -15,6 +15,6 @@
 </script>
 
 <form class="todo-form flex gap-4" onsubmit={handleSubmit}>
-	<input type="text" placeholder="Add a todo" bind:value={todoTitle} />
+	<input type="text" placeholder="Add a todo" bind:value={todoTitle} required />
 	<button class="bg-amber-400 w-32 text-black" type="submit">create new todo</button>
 </form>
