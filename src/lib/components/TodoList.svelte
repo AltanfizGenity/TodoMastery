ee6a3cdb7d68b7d1b7f66be0e7a98c3d146469e4
Altanfiz <altanfizgenity@gmail.com>
2025-08-15 15:21:12 +0700

    refactor(todo): extract todo list into component

---
+++ src/lib/components/TodoList.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { todos } from '$lib/store/todo';
+	import TodoItem from './TodoItem.svelte';
+</script>
+
+<div class="todo-list">
+	{#each $todos as todo}
+		<TodoItem currentTodo={todo} />
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import TodoForm from '$lib/components/TodoForm.svelte';
-	import TodoItem from '$lib/components/TodoItem.svelte';
-	import { todos } from '$lib/store/todo';
+	import TodoList from '$lib/components/TodoList.svelte';
 </script>
 
 <div class="todo-container">
 	<h1>Todos</h1>
-	<div class="todo-list">
-		{#each $todos as todo}
-			<TodoItem currentTodo={todo} />
-		{/each}
-	</div>
+	<TodoList />
 	<TodoForm />
 </div>
