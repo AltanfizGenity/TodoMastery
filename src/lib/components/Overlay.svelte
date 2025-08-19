4b14e9d27f86a1187223004e769cc0e991aec9a3
Altanfiz <altanfizgenity@gmail.com>
2025-08-19 19:30:57 +0700

    feat(overlay): add overlay component

---
+++ src/lib/components/Overlay.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	interface OverlayProps extends Props {
+		isActive: boolean;
+		onClosed: () => void;
+	}
+
+	let { isActive, children }: OverlayProps = $props();
+</script>
+
+{#if isActive}
+	<div
+		class="overlay fixed w-full min-h-screen inset-0 flex justify-center items-center z-100 bg-gray-800/25 backdrop-blur-sm"
+	>
+		{@render children()}
+	</div>
+{/if}
