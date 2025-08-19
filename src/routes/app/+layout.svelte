d58b5eea401911d2682de01dee000ecafffc5d20
Altanfiz <altanfizgenity@gmail.com>
2025-08-19 16:46:59 +0700

    refactor(app): relocate todo structure (v0.2.0) under app route

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="app-container flex">
+	<aside class="sidebar w-56 border-r border-1 border-amber-100">
+		<ul class="sidebar-navigation-list">
+			<li>
+				<a href="/tasks">tasks</a>
+			</li>
+		</ul>
+	</aside>
+	<main>
+		{@render children()}
+	</main>
+</div>
