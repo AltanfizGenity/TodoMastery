8c34d704be0ebbfe3f5c573d49bcdce168ae29f6
Altanfiz <altanfizgenity@gmail.com>
2025-08-21 08:08:23 +0700

    style(todo-item): move complete button to the left side

--- src/lib/components/TodoItem.svelte
+++ src/lib/components/TodoItem.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { completeTodo, deleteTodo } from '$lib/store/todo';
-	import { CircleCheck, Trash, type IconProps } from '@lucide/svelte';
+	import { Circle, CircleCheck, Trash, type IconProps } from '@lucide/svelte';
 	import { DateTime } from 'luxon';
 	import type { Component } from 'svelte';
 
@@ -16,9 +16,9 @@
 	let isOverdue = $derived(dueDateTime ? dueDateTime < DateTime.now() : false);
 </script>
 
-{#snippet action(callback: () => void, Icon: Component<IconProps>)}
+{#snippet action(callback: () => void, Icon: Component<IconProps>, className?: string)}
 	<button
-		class="cursor-pointer flex items-center justify-center hover:text-amber-500"
+		class={`cursor-pointer flex items-center justify-center hover:text-amber-500 ${className}`}
 		onclick={callback}
 	>
 		<Icon size={iconSize} />
@@ -26,13 +26,24 @@
 {/snippet}
 
 <div class="todo flex justify-between hover:bg-gray-50 p-2 cursor-pointer group">
-	<div class="todo-information">
+	<div class="todo-information flex gap-2 items-center">
+		<div class="complete-action group/complete-action hidden group-hover:flex">
+			{@render action(
+				() => completeTodo(currentTodo.id),
+				Circle,
+				'group-hover/complete-action:hidden'
+			)}
+			{@render action(
+				() => completeTodo(currentTodo.id),
+				CircleCheck,
+				'hidden group-hover/complete-action:flex'
+			)}
+		</div>
 		<h3 class={`${currentTodo.completed ? 'line-through' : ''} group-hover:text-amber-500`}>
 			{currentTodo.title}
 		</h3>
 	</div>
 	<div class="todo-actions hidden group-hover:flex gap-2">
-		{@render action(() => completeTodo(currentTodo.id), CircleCheck)}
 		{@render action(() => deleteTodo(currentTodo.id), Trash)}
 	</div>
 </div>
