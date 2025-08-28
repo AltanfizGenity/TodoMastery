1872496407576ffc2914405b0f6b147bc10f6227
Altanfiz <altanfizgenity@gmail.com>
2025-08-28 11:20:46 +0700

    feat(todoform): decouple deadline input into component

---
+++ src/lib/components/forms/TodoDeadlineInput.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { DateTime } from 'luxon';
+
+	interface InputProps {
+		dueDate: string;
+	}
+
+	let { dueDate = $bindable() }: InputProps = $props();
+</script>
+
+<input type="date" bind:value={dueDate} min={DateTime.now().toISODate()} />
