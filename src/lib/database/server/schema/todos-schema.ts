d533537639c33436e5b51f2ff4aebe3417eb20f1
Altanfiz <altanfizgenity@gmail.com>
2025-08-29 19:21:20 +0700

    feat(database): add todo schema and Drizzle config

---
+++ src/lib/database/server/schema/todos-schema.ts
@@ -0,0 +1,10 @@
+import { boolean, date, integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const todosTable = pgTable('todos', {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	title: text().notNull(),
+	category: text(),
+	dueDate: date(),
+	completed: boolean().notNull().default(false),
+	createdAt: timestamp().defaultNow().notNull()
+});
