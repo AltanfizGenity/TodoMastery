5ad37e73d3d4b0ae17e619ea6f5ca3e1e662b1f5
Altanfiz <altanfizgenity@gmail.com>
2025-09-01 08:08:13 +0700

    refactor(api): use fetch for modularity

--- src/routes/api/todos/+server.ts
+++ src/routes/api/todos/+server.ts
@@ -2,6 +2,11 @@ import { db } from '$lib/database/server/index.js';
 import { todosTable } from '$lib/database/server/schema/todos-schema.js';
 import { json } from '@sveltejs/kit';
 
+export async function GET() {
+	const todos = await db.select().from(todosTable);
+	return json(todos);
+}
+
 export async function POST({ request }) {
 	const data = (await request.json()) as typeof todosTable.$inferInsert;
 	const todos = await db.insert(todosTable).values(data).returning();
--- src/routes/app/todos/+page.server.ts
+++ src/routes/app/todos/+page.server.ts
@@ -1,7 +1,6 @@
-import { db } from '$lib/database/server';
-import { todosTable } from '$lib/database/server/schema/todos-schema';
-export async function load() {
-	const todos = await db.select().from(todosTable);
+export async function load({ fetch }) {
+	const response = await fetch('/api/todos');
+	const todos = await response.json();
 	return {
 		todos,
 		pageTitle: 'todos'
