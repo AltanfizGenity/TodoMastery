cafe4aaace0171d537bb86aa7952ea5c6da32ad5
Altanfiz <altanfizgenity@gmail.com>
2025-09-03 19:41:59 +0700

    refactor(todos): unify todo list component usage in completed and today pages

--- src/routes/app/completed/+page.svelte
+++ src/routes/app/completed/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import TodoCompletedList from '$lib/components/TodoCompletedList.svelte';
+	import TodoList from '$lib/components/TodoList.svelte';
 	import { todos } from '$lib/store/todo.js';
+	import { getCompletedTodos } from '$lib/utils/todos.js';
 	import { onMount } from 'svelte';
 
 	let { data } = $props();
+	let completedTodos = $derived(getCompletedTodos($todos));
 
 	onMount(() => {
 		todos.set(data.todos);
@@ -12,5 +14,5 @@
 
 <div class="todo-container flex flex-col gap-8">
 	<h1 class="text-2xl font-bold">Completed Todos</h1>
-	<TodoCompletedList />
+	<TodoList todos={completedTodos} emptyMessage="You haven't completed any todo yet" />
 </div>
--- src/routes/app/today/+page.svelte
+++ src/routes/app/today/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import TodoTodayList from '$lib/components/TodoTodayList.svelte';
+	import TodoList from '$lib/components/TodoList.svelte';
 	import { todos } from '$lib/store/todo.js';
+	import { getTodayTodos } from '$lib/utils/todos.js';
 	import { onMount } from 'svelte';
 
 	let { data } = $props();
+	let todayTodos = $derived(getTodayTodos($todos));
 
 	onMount(() => {
 		todos.set(data.todos);
@@ -12,5 +14,5 @@
 
 <div class="todo-container flex flex-col gap-8">
 	<h1 class="text-2xl font-bold">Today</h1>
-	<TodoTodayList />
+	<TodoList todos={todayTodos} emptyMessage="There nothing to do" />
 </div>
