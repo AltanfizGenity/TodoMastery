325da6eef0f842b9f73af2546a490f0adac4cc6d
Altanfiz <altanfizgenity@gmail.com>
2025-09-04 07:51:07 +0700

    feat(TodoProperty): use new update todo function for modify todo property

--- src/lib/components/TodoProperty.svelte
+++ src/lib/components/TodoProperty.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { updateTodoDB } from '$lib/api/db/todos/modify';
 	import type { Todo } from '$lib/database/server/schema/todos-schema';
 	import { isTodoPropertyOpen, todoPropertyId } from '$lib/store/appstate';
 	import { todos } from '$lib/store/todo';
@@ -18,16 +19,28 @@
 		todoPropertyId.set(null);
 	}
 
-	function changeTodoTitle(newTitle: string) {
-		console.log(newTitle);
+	async function changeTodoTitle(newTitle: string) {
+		await updateTodoProperty('title', newTitle);
 	}
 
-	function changeDueDate(newDate: string) {
-		console.log(newDate);
+	async function changeDueDate(newDate: string) {
+		await updateTodoProperty('dueDate', newDate);
 	}
 
-	function changeCategory(newCategory: string) {
-		console.log(newCategory);
+	async function changeCategory(newCategory: string) {
+		await updateTodoProperty('category', newCategory);
+	}
+
+	async function updateTodoProperty<K extends keyof Todo>(key: K, value: Todo[K]) {
+		let updatedField = { [key]: value };
+
+		if (!selectedTodo) {
+			return;
+		}
+
+		let response = await updateTodoDB(selectedTodo.id, updatedField);
+		let updatedTodo = response.data as Todo;
+		todos.update((prev) => prev.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo)));
 	}
 </script>
 
