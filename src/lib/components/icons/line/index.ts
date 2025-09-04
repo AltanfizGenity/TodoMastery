63ee4b621272e263978d4bee6b19fe6871556b76
Altanfiz <altanfizgenity@gmail.com>
2025-09-04 08:32:28 +0700

    feat(icons): add close line

--- src/lib/components/icons/line/index.ts
+++ src/lib/components/icons/line/index.ts
@@ -1,7 +1,16 @@
 import CircleCheckLine from './CircleCheckLine.svelte';
 import CircleLine from './CircleLine.svelte';
 import CloseCircleLine from './CloseCircleLine.svelte';
+import CloseLine from './CloseLine.svelte';
 import CompleteLine from './CompleteLine.svelte';
 import TodoLine from './TodoLine.svelte';
 import TrashLine from './TrashLine.svelte';
-export { CircleCheckLine, CircleLine, TrashLine, CloseCircleLine, CompleteLine, TodoLine };
+export {
+	CircleCheckLine,
+	CircleLine,
+	TrashLine,
+	CloseCircleLine,
+	CompleteLine,
+	TodoLine,
+	CloseLine
+};
