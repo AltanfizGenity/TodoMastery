c779c5587998209b1c40720b618b4b4a8e28195d
Altanfiz <altanfizgenity@gmail.com>
2025-10-03 12:31:09 +0700

    feat(auth): add register page

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { page } from '$app/state';
+</script>
+
+<form method="POST" use:enhance>
+	<input name="username" type="text" placeholder="username" required />
+	<input name="email" type="email" placeholder="email" required />
+	<input name="password" type="password" placeholder="password" required />
+	<button type="submit">Register</button>
+</form>
+
+{#if page.form?.message}
+	<p>{page.form.message}</p>
+{/if}
