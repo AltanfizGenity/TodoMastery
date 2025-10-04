8e11090a0c7ae4f4a76b3f3430e744a614e6ea7a
Altanfiz <altanfizgenity@gmail.com>
2025-10-04 09:29:56 +0700

    feat(auth): add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { page } from '$app/state';
+</script>
+
+<h1>Login to Todomastery</h1>
+<form method="POST" use:enhance action="?/login">
+	<input type="email" name="email" placeholder="example@mail.com" required />
+	<input type="password" name="password" placeholder="password" required />
+	<button type="submit">Login</button>
+</form>
+
+{#if page.form?.message}
+	<p>{page.form.message}</p>
+{/if}
