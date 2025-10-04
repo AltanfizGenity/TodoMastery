a6dc546a1d3982aa1555dbdae60443dfdf6aee1c
Altanfiz <altanfizgenity@gmail.com>
2025-10-04 10:25:54 +0700

    fix(jwt): correct token signing secrets and improve refresh token handling

--- src/lib/utils/jwt.ts
+++ src/lib/utils/jwt.ts
@@ -1,18 +1,30 @@
 import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from '$env/static/private';
-import { SignJWT, jwtVerify } from 'jose';
+import type { Cookies } from '@sveltejs/kit';
+import { SignJWT } from 'jose';
 
-const secret = new TextEncoder().encode(REFRESH_TOKEN_SECRET);
+const refreshTokenSecret = new TextEncoder().encode(REFRESH_TOKEN_SECRET);
+const accessTokenSecret = new TextEncoder().encode(ACCESS_TOKEN_SECRET);
 
 export const createAccessToken = async (id: string) => {
 	return await new SignJWT({ id })
 		.setProtectedHeader({ alg: 'HS256' })
 		.setExpirationTime('1d')
-		.sign(secret);
+		.sign(accessTokenSecret);
 };
 
 export const createRefreshToken = async (id: string) => {
 	return await new SignJWT({ id })
 		.setProtectedHeader({ alg: 'HS256' })
 		.setExpirationTime('90d')
-		.sign(secret);
+		.sign(refreshTokenSecret);
+};
+
+export const sendRefreshToken = (cookies: Cookies, refreshToken: string) => {
+	cookies.set('refreshtoken', refreshToken, {
+		httpOnly: true,
+		secure: true,
+		sameSite: 'strict',
+		path: '/',
+		maxAge: 60 * 60 * 24 * 30
+	});
 };
