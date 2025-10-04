888acc9dc8aa480abeaa20932830970445282924
Altanfiz <altanfizgenity@gmail.com>
2025-10-04 10:24:49 +0700

    fix(auth): change logout endpoint method from POST to GET

--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit';
 
-export async function POST({ cookies }) {
+export async function GET({ cookies }) {
 	cookies.delete('refreshtoken', { path: '/' });
 	return json({ success: true, message: 'Logout successful' });
 }
