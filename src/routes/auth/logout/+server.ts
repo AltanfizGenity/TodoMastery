7e524b5b1c84cb7c27c18352da93d7ac3f2a06f9
Altanfiz <altanfizgenity@gmail.com>
2025-10-05 15:11:53 +0700

    fix(auth): logout should clear access token

--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -2,5 +2,6 @@ import { json } from '@sveltejs/kit';
 
 export async function GET({ cookies }) {
 	cookies.delete('refreshtoken', { path: '/' });
+	cookies.delete('accessToken', { path: '/' });
 	return json({ success: true, message: 'Logout successful' });
 }
