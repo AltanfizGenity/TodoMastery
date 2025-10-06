c00e30ffc9fff6a2eea77cea2554c59e7dfcb436
Altanfiz <altanfizgenity@gmail.com>
2025-10-06 16:19:12 +0700

    feat(database): update todos schema

--- src/lib/database/server/schema/todos-schema.ts
+++ src/lib/database/server/schema/todos-schema.ts
@@ -2,6 +2,7 @@ import { boolean, date, integer, pgTable, text } from 'drizzle-orm/pg-core';
 import { createInsertSchema } from 'drizzle-zod';
 import { categoriesTable } from './categories-schema';
 import { created_at, id, updated_at } from './common-schema';
+import { usersTable } from './users';
 
 export const todosTable = pgTable('todos', {
 	id,
@@ -9,6 +10,9 @@ export const todosTable = pgTable('todos', {
 	dueDate: date(),
 	completed: boolean().default(false).notNull(),
 	category_id: integer().references(() => categoriesTable.id, { onDelete: 'set null' }),
+	user_id: integer()
+		.notNull()
+		.references(() => usersTable.id, { onDelete: 'cascade' }),
 	created_at,
 	updated_at
 });
